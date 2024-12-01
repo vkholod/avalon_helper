48cb3301ff63ab023485b410e6a01449ca891b47
Volodymyr Kholodenko <vkholodenko@expediagroup.com>
2024-11-30 18:42:43 -0800

    Score and color map

--- js/fetch-units.js
+++ js/fetch-units.js
@@ -4,32 +4,82 @@ $(document).ready(function () {
     $.getJSON(jsonUrl, function (data) {
         const units = data.units;
 
-        units.forEach(unit => {
-                const unitParts = unit.unitId.split("-");
-                const building = unitParts[2];
-                const apartment = unitParts[3];
+        $('#units-header').text(`Available Units (${units.length})`);
 
+        units.forEach(unit => {
+            const unitParts = unit.unitId.split("-");
+            const building = unitParts[2];
+            const apartment = unitParts[3];
 
             const virtualTour = unit.virtualTour && unit.virtualTour.space
                 ? `<a href="${unit.virtualTour.space}" target="_blank">View Tour</a>`
                 : "N/A";
 
+            const availableDate = new Date(unit.availableDateUnfurnished);
+            const year = availableDate.getFullYear();
+            const month = availableDate.toLocaleString('en-US', { month: 'short' });
+            const day = availableDate.getDate().toString();
+            const formattedDate = `${day} ${month} ${year}`;
+
+            let score = 0;
+
+            let floorClass;
+            if (unit.floorNumber == 1) {
+                floorClass = 'highlight-red';
+                score -= 1;
+            } else {
+                floorClass = 'highlight-green';
+                score += 2;
+            }
+
+            const price = parseFloat(unit.startingAtPricesUnfurnished.prices.price);
+            let priceClass;
+            if (price > 3200) {
+                priceClass = 'highlight-red';
+                score -= 3;
+            } else if (price < 2800) {
+                priceClass = 'highlight-green';
+                score += 1;
+            } else {
+                priceClass = 'highlight-yellow';
+            }
+
+            let buildingClass;
+            if (building.includes('04') || building.includes('06')) {
+                buildingClass = 'highlight-green';
+                score += 2;
+            } else {
+                buildingClass = 'highlight-yellow';
+            }
+
+            let squareFeetClass;
+            if (unit.squareFeet < 1000) {
+                squareFeetClass = 'highlight-red';
+                score -= 3;
+            } else if (unit.squareFeet > 1100) {
+                squareFeetClass = 'highlight-green';
+                score += 3;
+            } else {
+                squareFeetClass = 'highlight-yellow';
+            }
+
+
             const row = `
                 <tr>
-                    <td>${building}</td>
+                    <td>${score}</td>
+                    <td class="${buildingClass}">${building}</td>
                     <td>${apartment}</td>
                     <td>
                         <a href="https://resource.avalonbay.com/${unit.floorPlan.highResolution}" target="_blank">
                             <img src="https://resource.avalonbay.com/${unit.floorPlan.lowResolution}">
                         </a>
                     </td>
-                    <td>${unit.floorNumber}</td>
+                    <td class="${floorClass}">${unit.floorNumber}</td>
                     <td>${virtualTour}</td>
                     <td>${unit.bedroomNumber}</td>
-                    <td>${unit.bathroomNumber}</td>
-                    <td>${unit.squareFeet}</td>
-                    <td>${new Date(unit.availableDateUnfurnished).toISOString().split('T')[0]}</td>
-                    <td>$${unit.startingAtPricesUnfurnished.prices.price}</td>
+                    <td class="${squareFeetClass}">${unit.squareFeet}</td>
+                    <td>${formattedDate}</td>
+                    <td class="${priceClass}">${price}</td>
                     <td>${unit.startingAtPricesUnfurnished.leaseTerm} months</td>
                 </tr>
             `;
@@ -42,7 +92,8 @@ $(document).ready(function () {
                 pageLength: 50,
                 lengthMenu: [10, 25, 50, 100],
                 order: [
-                    [7, 'desc']
+                    [0, 'desc'],
+                    [7, 'desc'],
                 ],
                 orderCellsTop: true,
                 initComplete: function () {
